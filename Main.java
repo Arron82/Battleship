01cee96a133001ff70c4068b1a82d5e051913c1d
Arron Pelc <52333841+Arron82@users.noreply.github.com>
2019-12-09 23:17:58 -0600

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,13 @@
+/* battlship.java calls actiongame.java.  This is where the main method resides
+ * 
+ * Author:	Arron Pelc
+ * Date:	12/1/2019*/
+ 
+ 
+public class Main
+{
+	public static void main(String[] args)
+	{
+		actiongame.actionGame();
+	}
+}
